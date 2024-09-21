bbfae912838e61f0f040b853acf7728752b922c6
Tim1222 <timofeu47@gmail.com>
2024-09-21 14:29:57 +0300

    add Input Story to un controlled components

---
+++ src/Components/Stories/Input.stories.tsx
@@ -0,0 +1,35 @@
+import {ChangeEvent, useRef, useState} from "react";
+
+export default {
+    title: 'input',
+    // component: input
+}
+
+export const UncontrolledInput = () => <input/>
+
+export const ControlledInputWithFixedValue = () => <input value={'some input'}/>
+
+export const TrackValueOfUncontrolledInput = () => {
+    const [value, setValue] = useState('')
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.currentTarget.value)
+    }
+
+    return <><input onChange={onChange}/> - {value}</>
+}
+
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState('')
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const save = () => {
+        const el = inputRef.current as HTMLInputElement
+        setValue(el.value)
+    }
+
+    return <><input ref={inputRef}/>
+        <button onClick={save}>save</button>
+         actual value: {value}</>
+}
+
+
